ce8fac4384936ab2479250992892f894280c37e7
droelofsen <droelofsen@sogyo.nl>
2024-12-10 12:07:18 +0100

    added Sensor functionality to test out

--- src/main/allSensors.py
+++ src/main/allSensors.py
@@ -116,11 +116,11 @@ try:
                 if justbumped:
                     justbumped = False
                     if (BP.get_sensor(BP.PORT_1) < 40):
-                        turntime = 10
+                        turntime = 9
                         speedleft = 30
                         speedright = -30
                     else:
-                        turntime = 10
+                        turntime = 9
                         speedleft = -30
                         speedright = 30
 

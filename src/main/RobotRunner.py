6af500fbf20c14724a7606326b7f4da5d99cb038
droelofsen <droelofsen@sogyo.nl>
2024-12-11 09:37:22 +0100

    fixed error in naming

--- src/main/RobotRunner.py
+++ src/main/RobotRunner.py
@@ -108,20 +108,24 @@ def manual_driving(bp, key_states):
     speed_right = 0
     speed_blade = 0
 
-    if key_states["up_is_pressed"]:
+    if key_states["up"]:
         speed_left -= 60
         speed_right -= 60
-    if key_states["down_is_pressed"]:
+    if key_states["down"]:
         speed_left += 60
         speed_right += 60
-    if key_states["right_is_pressed"]:
+    if key_states["right"]:
         speed_right += 20
         speed_left -= 20
-    if key_states["left_is_pressed"]:
+    if key_states["left"]:
         speed_right -= 20
         speed_left += 20
-    if key_states["space_is_pressed"]:
+    if key_states["space"]:
         speed_blade = 100
+    if key_states["lshift"]:
+        speed_left = 0
+        speed_right = 0
+        speed_blade = 0
 
     set_motor_power(bp, speed_left, speed_right)
     set_blade_power(bp, speed_blade)

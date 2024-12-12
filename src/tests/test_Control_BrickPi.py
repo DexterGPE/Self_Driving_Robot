5bb7cff74ee7a40c70b6904c7b4bab850f181c40
droelofsen <droelofsen@sogyo.nl>
2024-12-12 14:26:57 +0000

    added tests

--- src/tests/test_Control_BrickPi.py
+++ src/tests/test_Control_BrickPi.py
@@ -1,9 +1,31 @@
 import unittest
+import brickpi3
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
+class TestBrickPi3Functions(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize the BrickPi3 instance for testing
+        self.bp = brickpi3.BrickPi3()
+
+    def test_set_motor_power(self):
+        # Test setting motor power
+        set_motor_power(self.bp, 100, 200)
+        self.assertEqual(self.bp.get_motor_power(self.bp.PORT_D), 100)
+        self.assertEqual(self.bp.get_motor_power(self.bp.PORT_A), 200)
+
+    def test_set_blade_power(self):
+        # Test setting blade power
+        set_blade_power(self.bp, 150)
+        self.assertEqual(self.bp.get_motor_power(self.bp.PORT_B), 150)
+
+    def test_initialize_brickpi_sensors(self):
+        # Test sensor initialization
+        bp = initialize_brickpi_sensors()
+        self.assertEqual(bp.get_sensor_type(bp.PORT_1), bp.SENSOR_TYPE.EV3_INFRARED_PROXIMITY)
+        self.assertEqual(bp.get_sensor_type(bp.PORT_2), bp.SENSOR_TYPE.TOUCH)
+        self.assertEqual(bp.get_sensor_type(bp.PORT_3), bp.SENSOR_TYPE.TOUCH)
+        self.assertEqual(bp.get_sensor_type(bp.PORT_4), bp.SENSOR_TYPE.EV3_COLOR_COLOR_COMPONENTS)
 
 
 if __name__ == '__main__':

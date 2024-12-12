d43fe9972ccdb867af74f7e1b35648cc3bc7a6ed
Isja Mannens <isja-m@hotmail.com>
2024-12-12 14:33:35 +0100

    Tweaks.

--- src/main/Robot_Runner.py
+++ src/main/Robot_Runner.py
@@ -17,8 +17,8 @@ def print_sensors(bp, countdown):
         if countdown == 0:
             print("Wall distance: {0}".format(bp.get_sensor(bp.PORT_1)))
             print("Color: {0}".format(bp.get_sensor(bp.PORT_4)))
-            print("Left touch pushed: {0}".format(bp.get_sensor(bp.Port_3)))
-            print("Right touch pushed: {0}".format(bp.get_sensor(bp.Port_2)))
+            print("Left touch pushed: {0}".format(bp.get_sensor(bp.PORT_3)))
+            print("Right touch pushed: {0}".format(bp.get_sensor(bp.PORT_2)))
             countdown = 50
     except:
         print("Invalid sensor data")

bf56a59ecc89011abcfafe1fb4347fcdd6fe3fba
Isja Mannens <isja-m@hotmail.com>
2024-12-12 12:10:19 +0100

    Tweaks

--- src/main/Manual_Driving.py
+++ src/main/Manual_Driving.py
@@ -2,8 +2,8 @@ from RobotRunner import *
 
 
 def manual_driving(bp, key_states):
-    # print(bp.get_sensor(bp.PORT_1))
-    # print(bp.get_sensor(bp.PORT_4))
+    print(bp.get_sensor(bp.PORT_1))
+    print(bp.get_sensor(bp.PORT_4))
     speed_left = 0
     speed_right = 0
     speed_blade = 0

d64f1acc66dbf8a43af2b8f7b0742245db1dcec1
droelofsen <droelofsen@sogyo.nl>
2024-12-13 15:17:24 +0100

    tweaking

--- src/main/Robot_Runner.py
+++ src/main/Robot_Runner.py
@@ -69,6 +69,5 @@ if __name__ == "__main__":
         elif key_states["mode"] == 9:
             speed_left, speed_right, wall_finding, time_since_black_line, key_states["mode"] = Smooth_Operator.self_driving(
                 bp,speed_left, speed_right, wall_finding, time_since_black_line, 5, 2.6, -30, 17, 18, key_states["mode"])
-
         time.sleep(0.02)
     

528ad3d6e1a8d8d890d187614a58b2152616d270
droelofsen <droelofsen@sogyo.nl>
2024-12-16 13:06:02 +0100

    making the robot go slower on ramp to not make it fall off

--- src/main/sport_mode.py
+++ src/main/sport_mode.py
@@ -59,4 +59,4 @@ def smooth_left_turn_on_bridge(pars):
 def smooth_right_turn_on_bridge(pars):
     speed_left = pars["standard_speed"] - pars["turn_speed"]
     speed_right = pars["standard_speed"] + pars["turn_speed"]
-    return speed_left/1.5, speed_right/1.5
+    return speed_left/2.5, speed_right/2.5

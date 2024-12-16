a0afc75f0d1085dd802b7ed94267fd36ab61726e
droelofsen <droelofsen@sogyo.nl>
2024-12-16 09:21:15 +0100

    tweaking smooth bridge functions

--- src/main/Smooth_Operator.py
+++ src/main/Smooth_Operator.py
@@ -139,11 +139,11 @@ def smooth_right_turn_on_bridge(speed_left, speed_right, pars):
     print("adjustment = ", adjustment)
     # Adjust speeds for right turn (forward = negative, backward = positive)
     speed_left += adjustment  # Increase forward speed (less negative)
-    speed_right -= pars["standard_speed"] - adjustment  # Reduce forward speed (more negative)
+    speed_right -= adjustment  # Reduce forward speed (more negative)
     print(speed_left,speed_right,"left,right before")
     # Constrain speeds within reasonable bounds
-    # speed_left = max(pars["standard_speed"] + 2 * pars["turn_speed"], speed_left)
-    # speed_right = min(pars["standard_speed"] - 2 * pars["turn_speed"], speed_right)
+    speed_left = max(pars["standard_speed"] + 2 * pars["turn_speed"], speed_left)
+    speed_right = max(pars["standard_speed"] - 2 * pars["turn_speed"], speed_right)
     print(speed_left, speed_right, "left,right after")
     return speed_left, speed_right
 

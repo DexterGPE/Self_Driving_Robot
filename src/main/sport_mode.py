73a021f76acf0e197a118a517af20df42e686afe
droelofsen <droelofsen@sogyo.nl>
2024-12-16 16:29:27 +0100

    bugfix

--- src/main/sport_mode.py
+++ src/main/sport_mode.py
@@ -55,10 +55,10 @@ def self_driving(bp, speed_left, speed_right, wall_finding, time_since_red_line,
 def smooth_left_turn_on_bridge(pars):
     speed_left = pars["standard_speed"] - pars["turn_speed"]
     speed_right = pars["standard_speed"] + pars["turn_speed"]
-    return speed_left/2.5, speed_right/2.5
+    return speed_left, speed_right
 
 
 def smooth_right_turn_on_bridge(pars):
     speed_left = pars["standard_speed"] + pars["turn_speed"]
     speed_right = pars["standard_speed"] - pars["turn_speed"]
-    return speed_left/2.5, speed_right/2.5
+    return speed_left, speed_right

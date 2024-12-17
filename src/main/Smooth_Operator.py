12c038f9d70abaf1caa15951cf5b1ad980d3281e
Dexter Roelofsen <droelofsen@sogyo.nl>
2024-12-17 11:09:47 +0000

    Changed sport_mode and smoot_operator to classes, with inheritance.
    uncomment print
    
    compactified parameters
    
    removed wall_finding parameter
    
    removed redundant files and parameters.

--- src/main/Smooth_Operator.py
+++ src/main/Smooth_Operator.py
@@ -1,129 +1,127 @@
-import Self_Driving_Naive
 import Control_BrickPi
 import time
 
+class Smooth_Operator:
 
-def self_driving(bp, speed_left, speed_right, wall_finding, time_since_black_line, smoothness, bridgesmoothness,
-                 standard_speed, turn_speed, distance_to_wall, mode):
-    pars = {
-        "smoothness": smoothness,
-        "bridgesmoothness": bridgesmoothness,
-        "standard_speed": standard_speed,
-        "turn_speed": turn_speed,
-        "distance_to_wall": distance_to_wall
-    }
-    wall_finding -= 1  # Probably obsolete
-    time_since_black_line -= 1
-    blade_speed = 50
+    @classmethod
+    def self_driving(cls, bp, speed_left, speed_right, pars):
+        blade_speed = 50
 
-    try:
-        if detect_black(bp):
-            time_since_black_line = 50
-        if Self_Driving_Naive.bumped_into_wall(bp):
-            if detect_finish(bp, distance_to_wall):
-                speed_left = 0
-                speed_right = 0
-                mode = 0
-            else:
-                if is_right_wall_found(bp, distance_to_wall):
-                    turn_left_after_bump(bp, pars)
+        try:
+            if cls.bumped_into_wall(bp):
+                if cls.detect_finish(bp, pars["distance_to_wall"]):
+                    speed_left = 0
+                    speed_right = 0
+                    pars["mode"] = 0
                 else:
-                    turn_right_after_bump(bp, pars)
+                    if cls.is_right_wall_found(bp, pars["distance_to_wall"]):
+                        cls.turn_left_after_bump(bp, pars)
+                    else:
+                        cls.turn_right_after_bump(bp, pars)
 
-                speed_left = 0
-                speed_right = 0
-        elif red_line_found(bp) and get_right_wall_distance(bp) > 23:
-            wall_finding = 25
-            speed_left, speed_right = smooth_left_turn_on_bridge(speed_left, speed_right, pars)
-        elif get_right_wall_distance(bp) > 23:
-            speed_left, speed_right = smooth_right_turn_on_bridge(speed_left, speed_right, pars)
-        elif wall_finding < 0:
-            speed_left, speed_right = smooth_turn_at_wall(bp, pars)
+                    speed_left = 0
+                    speed_right = 0
+            elif cls.red_line_found(bp) and cls.get_right_wall_distance(bp) > 23:
+                speed_left, speed_right = cls.smooth_left_turn_on_bridge(speed_left, speed_right, pars)
+            elif cls.get_right_wall_distance(bp) > 23:
+                speed_left, speed_right = cls.smooth_right_turn_on_bridge(speed_left, speed_right, pars)
+            else:
+                speed_left, speed_right = cls.smooth_turn_at_wall(bp, pars)
 
-    except:
-        print("Invalid sensor data.")
-    Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
-    Control_BrickPi.set_blade_power(bp, blade_speed)
+        except:
+            print("Invalid sensor data.")
+        Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
+        Control_BrickPi.set_blade_power(bp, blade_speed)
 
-    return speed_left, speed_right, wall_finding, time_since_black_line, mode
+        return speed_left, speed_right, pars["mode"]
 
+    @classmethod
+    def turn_left_after_bump(cls, bp, pars):
+        cls.reverse_after_bump(bp, pars)
+        speed_left, speed_right = cls.turn_left(pars)
+        Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
+        time.sleep(1.55 * -30 / pars["standard_speed"])
 
-def turn_left_after_bump(bp, pars):
-    Self_Driving_Naive.reverse_after_bump(bp, pars)
-    speed_left, speed_right = turn_left(pars)
-    Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
-    time.sleep(1.55 * -30 / pars["standard_speed"])
+    @classmethod
+    def turn_right_after_bump(cls, bp, pars):
+        cls.reverse_after_bump(bp, pars)
+        speed_left, speed_right = cls.turn_right(pars)
+        Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
+        time.sleep(1.55 * -30 / pars["standard_speed"])
 
+    @staticmethod
+    def is_right_wall_found(bp, distance_to_wall):
+        return bp.get_sensor(bp.PORT_1) < 30
 
-def turn_right_after_bump(bp, pars):
-    Self_Driving_Naive.reverse_after_bump(bp, pars)
-    speed_left, speed_right = turn_right(pars)
-    Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
-    time.sleep(1.55 * -30 / pars["standard_speed"])
+    @staticmethod
+    def get_right_wall_distance(bp):
+        return bp.get_sensor(bp.PORT_1)
 
+    @classmethod
+    def smooth_turn_at_wall(cls, bp, pars):
+        distance = cls.get_right_wall_distance(bp)
+        correction_factor = max(-1, min(1, (distance - pars["distance_to_wall"]) / pars["smoothness"]))
+        speed_left = pars["standard_speed"] + pars["turn_speed"] * correction_factor
+        speed_right = pars["standard_speed"] - pars["turn_speed"] * correction_factor
+        return speed_left, speed_right
 
-def is_right_wall_found(bp, distance_to_wall):
-    return bp.get_sensor(bp.PORT_1) < 30
+    @staticmethod
+    def smooth_left_turn_on_bridge(speed_left, speed_right, pars):
+        speed_left = min(pars["standard_speed"] - pars["turn_speed"],
+                         speed_left - (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+        speed_left = min(speed_left, pars["standard_speed"] - pars["turn_speed"])
 
+        speed_right = max(pars["standard_speed"] + pars["turn_speed"],
+                          speed_right + (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+        speed_right = min(speed_right, pars["standard_speed"] - pars["turn_speed"])
 
-def get_right_wall_distance(bp):
-    return bp.get_sensor(bp.PORT_1)
+        return speed_left, speed_right
 
+    @staticmethod
+    def smooth_right_turn_on_bridge(speed_left, speed_right, pars):
+        speed_left = max(pars["standard_speed"] + pars["turn_speed"],
+                         speed_left + (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+        speed_left = min(speed_left, pars["standard_speed"] - pars["turn_speed"])
 
-def smooth_turn_at_wall(bp, pars):
-    distance = get_right_wall_distance(bp)
-    correction_factor = max(-1, min(1, (distance - pars["distance_to_wall"]) / pars["smoothness"]))
-    speed_left = pars["standard_speed"] + pars["turn_speed"] * correction_factor
-    speed_right = pars["standard_speed"] - pars["turn_speed"] * correction_factor
-    return speed_left, speed_right
+        speed_right = min(pars["standard_speed"] - pars["turn_speed"],
+                          speed_right - (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+        speed_right = min(speed_right, pars["standard_speed"] - pars["turn_speed"])
 
+        return speed_left, speed_right
 
-def smooth_left_turn_on_bridge(speed_left, speed_right, pars):
-    speed_left = min(pars["standard_speed"] - pars["turn_speed"],
-                     speed_left - (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+    @staticmethod
+    def red_line_found(bp):
+        return bp.get_sensor(bp.PORT_4)[0] > 1.7 * bp.get_sensor(bp.PORT_4)[1] and bp.get_sensor(bp.PORT_4)[0] > 2.5 * \
+            bp.get_sensor(bp.PORT_4)[2]
 
-    speed_left = min(speed_left, pars["standard_speed"] - pars["turn_speed"])
+    @staticmethod
+    def turn_left(pars):
+        speed_left = -pars["standard_speed"]
+        speed_right = pars["standard_speed"]
+        return speed_left, speed_right
 
-    speed_right = max(pars["standard_speed"] + pars["turn_speed"],
-                      speed_right + (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
+    @staticmethod
+    def turn_right(pars):
+        speed_left = pars["standard_speed"]
+        speed_right = -pars["standard_speed"]
+        return speed_left, speed_right
 
-    speed_right = min(speed_right, pars["standard_speed"] - pars["turn_speed"])
+    @staticmethod
+    def detect_black(bp):
+        return (bp.get_sensor(bp.PORT_4)[0] < 35) and (bp.get_sensor(bp.PORT_4)[1] < 45) and (
+                bp.get_sensor(bp.PORT_4)[2] < 25)
 
-    return speed_left, speed_right
+    @classmethod
+    def detect_finish(cls, bp, distance_to_wall):
+        return cls.detect_black(bp) and cls.is_right_wall_found(bp, distance_to_wall)
 
+    @staticmethod
+    def bumped_into_wall(bp):
+        return bp.get_sensor(bp.PORT_2) or bp.get_sensor(bp.PORT_3)
 
-def smooth_right_turn_on_bridge(speed_left, speed_right, pars):
-    speed_left = max(pars["standard_speed"] + pars["turn_speed"],
-                     speed_left + (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
-    speed_left = min(speed_left, pars["standard_speed"] - pars["turn_speed"])
-    speed_right = min(pars["standard_speed"] - pars["turn_speed"],
-                      speed_right - (pars["turn_speed"] / (pars["bridgesmoothness"] * pars["smoothness"])))
-    speed_right = min(speed_right, pars["standard_speed"] - pars["turn_speed"])
-
-    return speed_left, speed_right
-
-
-def red_line_found(bp):
-    return bp.get_sensor(bp.PORT_4)[0] > 1.7 * bp.get_sensor(bp.PORT_4)[1] and bp.get_sensor(bp.PORT_4)[0] > 2.5 * \
-        bp.get_sensor(bp.PORT_4)[2]
-
-
-def turn_left(pars):
-    speed_left = -pars["standard_speed"]
-    speed_right = pars["standard_speed"]
-    return speed_left, speed_right
-
-
-def turn_right(pars):
-    speed_left = pars["standard_speed"]
-    speed_right = -pars["standard_speed"]
-    return speed_left, speed_right
-
-
-def detect_black(bp):
-    return (bp.get_sensor(bp.PORT_4)[0] < 35) and (bp.get_sensor(bp.PORT_4)[1] < 45) and (
-            bp.get_sensor(bp.PORT_4)[2] < 25)
-
-
-def detect_finish(bp, distance_to_wall):
-    return detect_black(bp) and is_right_wall_found(bp, distance_to_wall)
+    @staticmethod
+    def reverse_after_bump(bp, pars):
+        speed_left = -pars["standard_speed"]
+        speed_right = -pars["standard_speed"]
+        Control_BrickPi.set_motor_power(bp, speed_left, speed_right)
+        time.sleep(0.40 * -30 / pars["standard_speed"])
